165c307fd7ce1c7eecd4d4c6460878ed89543965
kvs vishnu kumar <kvsvishnukumar@kvss-Mac-mini.local>
2025-08-15 19:44:32 +0530

    Boot dev http server chapter1

---
+++ boot_dev/http_server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				log.Println("File ended !!")
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		// when file doesnot end with a new line
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
+
+func main() {
+	f, err := os.Open("messages.txt")
+
+	if err != nil {
+		log.Fatal("Not able to open the file")
+	}
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Println(line)
+	}
+
+}
+
+// Notes
+
+// https://www.youtube.com/watch?v=FknTw9bJsXM
+
+// 1. f is a file pointer. When ever you do f.Read() it goes to next from prev position. Thats how it advances
+// 2. second task is to still read data of 8 bytes but print every newline occurence. So we read until we find a newline using bytes.IndexByte() function. Print and clean
+// 3. <-chan string : is a receive only channel of strings. You create a seperate fucntion with channels and goroutines and send each line to out channel
+// 4.
