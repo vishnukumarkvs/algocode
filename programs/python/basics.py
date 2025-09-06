41d00d4f4f86da8164511f46a233fe3b4215437d
kvs vishnu kumar <kvsvishnukumar@kvss-Mac-mini.local>
2025-09-06 14:27:30 +0530

    added sorts

--- programs/python/basics.py
+++ programs/python/basics.py
@@ -71,7 +71,7 @@ def get_avg_brand_followers(all_handles, brand_name):
     for l1 in all_handles:
         for l2 in l1:
             if "cosmo" in l2:
-                cnt++
+                cnt+=1
     lists = len(all_handles)
 
     return cnt/lists
@@ -93,6 +93,78 @@ def binary_search(nums, val):
             high = mid -1
     return -1
 
+# compare adjacent elements
+def bubble_sort(nums):
+    n = len(nums)
+
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    print(nums)
+
+# find min and bring them to first(i)
+def selection_sort(nums):
+    n = len(nums)
+
+    for i in range(n-1):
+        minn = nums[i]
+        idx = i
+        for j in range(i+1,n):
+            if nums[j] < minn:
+                minn = nums[j]
+                idx = j
+        nums[i], nums[idx] = nums[idx], nums[i]
+    print(nums)
+
+# put an element in its place in left sorted part of array
+def insertion_sort(nums):
+    n = len(nums)
+    for i in range(1,n):
+        key = nums[i]
+        j = i - 1
+
+        if j>=0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            j-=1
+        nums[j+1] = key
+    print(nums)
+
+# Divide and cxonquer algorithdm . Best, Avg, Worst case is O(nlog(n))
+def merge_sort(nums):
+
+    # base case
+    if len(nums) <= 1:
+        return nums
+    
+    mid = len(nums) // 2
+
+    left_sorted = merge_sort(nums[:mid])
+    right_sorted = merge_sort(nums[mid:])
+
+    return merge(left_sorted, right_sorted)
+
+def merge(left, right):
+    result = []
+    i , j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+
 if __name__ == "__main__":
     print(find_minimum([1,2,3]))
     print(find_minimum([]))
@@ -102,3 +174,9 @@ if __name__ == "__main__":
     print(get_follower_prediction(10, "fitness", 10))
     print(get_influencer_score(2, 75))
     print(num_possible_orders(10))
+    print(bubble_sort([2,1,7,6]))
+    print(selection_sort([2,1,7,6]))
+    print(sorted([6,3,7,1]))
+    print(insertion_sort([2,1,7,6]))
+    print(merge_sort([3,6,2,55,11,2,77,89,32]))
+
